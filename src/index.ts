eab7c2126b5c3f7e096b434bdd5e28e234518250
Paulius Kiselis <paulius.kiselis@wahanda.com>
2018-10-10 16:17:57 +0300

    fixed not compiling code.

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,7 @@ import { User, Users, UsersLocal } from 'models/users';
 
 const web = new WebClient(env.SLACK_KEY);
 const scheduler = new Scheduler(web);
-scheduler.init();
-
+scheduler.start();
 
 const usersRepo = new UsersLocal()
 console.log("User by date", usersRepo.userByDate(new Date()), " additional stirng ");
